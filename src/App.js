56080094581292f4926dd9c80b3f32af4cbbb6e2
Bartosz Art <bartosz.art@gmail.com>
2021-12-25 19:22:05 +0100

    Modified App.js with under construcion page

--- src/App.js
+++ src/App.js
@@ -6,17 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Bartosz Art</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload xD.
+          My website will be here soon.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
