0c674f65a19799c35e328db0eae3a3e817a8a5bf
Bartosz Art <bartosz.art@gmail.com>
2022-01-05 17:57:26 +0100

    Added skills grid and changed footer position

--- src/components/other/AboutMe.js
+++ src/components/other/AboutMe.js
@@ -3,14 +3,125 @@ import React from 'react'
 function AboutMe() {
 	return (
 		<main>
-			<h2>ABOUT ME 1</h2>
-			<h2>ABOUT ME 2</h2>
-			<h2>ABOUT ME 3</h2>
-			<h2>ABOUT ME 4</h2>
-			<h2>ABOUT ME 5</h2>
-			<h2>ABOUT ME 6</h2>
-			<h2>ABOUT ME 7</h2>
-			<h2>ABOUT ME 8</h2>
+			<h2>Skills</h2>
+			<div className="skills">
+				<div className="skills-box">
+					<i className="fab fa-react"></i>
+					<p className="skills-box-text">React</p>
+				</div>
+				<div className="skills-box">
+					<i className="fab fa-node-js"></i>
+					<p className="skills-box-text">Node.js</p>
+				</div>
+				<div className="skills-box">
+					<i className="devicon-express-original"></i>
+					<p className="skills-box-text">Express</p>
+				</div>
+				<div className="skills-box">
+					<i className="devicon-sequelize-plain"></i>
+					<p className="skills-box-text">Sequelize</p>
+				</div>
+				<div className="skills-box">
+					<i className="devicon-dot-net-plain"></i>
+					<p className="skills-box-text">.NET</p>
+				</div>
+				<div className="skills-box">
+					<i className="devicon-dotnetcore-plain"></i>
+					<p className="skills-box-text">.NET Core</p>
+				</div>
+				<div className="skills-box">
+					<i className="devicon-wordpress-plain"></i>
+					<p className="skills-box-text">Wordpress</p>
+				</div>
+
+
+				<div className="skills-box">
+					<i className="devicon-csharp-plain"></i>
+					<p className="skills-box-text">C#</p>
+				</div>
+				<div className="skills-box">
+					<i className="fab fa-html5"></i>
+					<p className="skills-box-text">HTML5</p>
+				</div>
+				<div className="skills-box">
+					<i className="fab fa-css3-alt"></i>
+					<p className="skills-box-text">CSS3</p>
+				</div>
+				<div className="skills-box">
+					<i className="fab fa-js"></i>
+					<p className="skills-box-text">JavaScript</p>
+				</div>
+				<div className="skills-box">
+					<i className="fab fa-sass"></i>
+					<p className="skills-box-text">SASS</p>
+				</div>
+				<div className="skills-box">
+					<i className="devicon-java-plain"></i>
+					<p className="skills-box-text">Java</p>
+				</div>
+				<div className="skills-box">
+					<i className="devicon-microsoftsqlserver-plain"></i>
+					<p className="skills-box-text">Transact-SQL</p>
+				</div>
+				<div className="skills-box">
+					<i className="devicon-oracle-original"></i>
+					<p className="skills-box-text">PL/SQL</p>
+				</div>
+				<div className="skills-box">
+					<i className="devicon-php-plain"></i>
+					<p className="skills-box-text">php</p>
+				</div>
+				<div className="skills-box">
+					<i className="devicon-bash-plain"></i>
+					<p className="skills-box-text">Bash</p>
+				</div>
+
+
+				<div className="skills-box">
+					<i className="devicon-azure-plain"></i>
+					<p className="skills-box-text">Azure</p>
+				</div>
+				<div className="skills-box">
+					<i className="devicon-heroku-original"></i>
+					<p className="skills-box-text">Heroku</p>
+				</div>
+				<div className="skills-box">
+					<i className="fab fa-stack-overflow"></i>
+					<p className="skills-box-text">Stack Overflow</p>
+				</div>
+				<div className="skills-box">
+					<i className="fab fa-github"></i>
+					<p className="skills-box-text">GitHub</p>
+				</div>
+				<div className="skills-box">
+					<i className="devicon-git-plain"></i>
+					<p className="skills-box-text">Git</p>
+				</div>
+				<div className="skills-box">
+					<i className="devicon-docker-plain"></i>
+					<p className="skills-box-text">Docker</p>
+				</div>
+				<div className="skills-box">
+					<i className="devicon-mysql-plain"></i>
+					<p className="skills-box-text">MySQL</p>
+				</div>
+				<div className="skills-box">
+					<i className="devicon-visualstudio-plain"></i>
+					<p className="skills-box-text">Visual Studio</p>
+				</div>
+				<div className="skills-box">
+					<i className="devicon-vscode-plain"></i>
+					<p className="skills-box-text">Visual Studio Code</p>
+				</div>
+				<div className="skills-box">
+					<i className="devicon-putty-plain"></i>
+					<p className="skills-box-text">PuTTy</p>
+				</div>
+				<div className="skills-box">
+					<i className="devicon-windows8-original"></i>
+					<p className="skills-box-text">Windows 10</p>
+				</div>
+			</div>
 		</main>
 	)
 }
