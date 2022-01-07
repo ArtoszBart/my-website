2f1333a7c4b2998afab59ada2d76c90f28eeb0ed
Bartosz Art <bartosz.art@gmail.com>
2022-01-07 16:20:33 +0100

    Minor changes

--- src/components/other/AboutMe.js
+++ src/components/other/AboutMe.js
@@ -12,119 +12,119 @@ function AboutMe() {
 				<h2>Skills</h2>
 				<div className="skills">
 					<div className="skills-box">
-						<i className="fab fa-react"></i>
+						<i className="skills-box-logo fab fa-react"></i>
 						<p className="skills-box-text">React</p>
 					</div>
 					<div className="skills-box">
-						<i className="fab fa-node-js"></i>
+						<i className="skills-box-logo fab fa-node-js"></i>
 						<p className="skills-box-text">Node.js</p>
 					</div>
 					<div className="skills-box">
-						<i className="devicon-express-original"></i>
+						<i className="skills-box-logo devicon-express-original"></i>
 						<p className="skills-box-text">Express</p>
 					</div>
 					<div className="skills-box">
-						<i className="devicon-sequelize-plain"></i>
+						<i className="skills-box-logo devicon-sequelize-plain"></i>
 						<p className="skills-box-text">Sequelize</p>
 					</div>
 					<div className="skills-box">
-						<i className="devicon-dot-net-plain"></i>
+						<i className="skills-box-logo devicon-dot-net-plain"></i>
 						<p className="skills-box-text">.NET</p>
 					</div>
 					<div className="skills-box">
-						<i className="devicon-dotnetcore-plain"></i>
+						<i className="skills-box-logo devicon-dotnetcore-plain"></i>
 						<p className="skills-box-text">.NET Core</p>
 					</div>
 					<div className="skills-box">
-						<i className="devicon-wordpress-plain"></i>
+						<i className="skills-box-logo devicon-wordpress-plain"></i>
 						<p className="skills-box-text">Wordpress</p>
 					</div>
 
 
 					<div className="skills-box">
-						<i className="devicon-csharp-plain"></i>
+						<i className="skills-box-logo devicon-csharp-plain"></i>
 						<p className="skills-box-text">C#</p>
 					</div>
 					<div className="skills-box">
-						<i className="fab fa-html5"></i>
+						<i className="skills-box-logo fab fa-html5"></i>
 						<p className="skills-box-text">HTML5</p>
 					</div>
 					<div className="skills-box">
-						<i className="fab fa-css3-alt"></i>
+						<i className="skills-box-logo fab fa-css3-alt"></i>
 						<p className="skills-box-text">CSS3</p>
 					</div>
 					<div className="skills-box">
-						<i className="fab fa-js"></i>
+						<i className="skills-box-logo fab fa-js"></i>
 						<p className="skills-box-text">JavaScript</p>
 					</div>
 					<div className="skills-box">
-						<i className="fab fa-sass"></i>
+						<i className="skills-box-logo fab fa-sass"></i>
 						<p className="skills-box-text">SASS</p>
 					</div>
 					<div className="skills-box">
-						<i className="devicon-java-plain"></i>
+						<i className="skills-box-logo devicon-java-plain"></i>
 						<p className="skills-box-text">Java</p>
 					</div>
 					<div className="skills-box">
-						<i className="devicon-microsoftsqlserver-plain"></i>
+						<i className="skills-box-logo devicon-microsoftsqlserver-plain"></i>
 						<p className="skills-box-text">Transact-SQL</p>
 					</div>
 					<div className="skills-box">
-						<i className="devicon-oracle-original"></i>
+						<i className="skills-box-logo devicon-oracle-original"></i>
 						<p className="skills-box-text">PL/SQL</p>
 					</div>
 					<div className="skills-box">
-						<i className="devicon-php-plain"></i>
+						<i className="skills-box-logo devicon-php-plain"></i>
 						<p className="skills-box-text">php</p>
 					</div>
 					<div className="skills-box">
-						<i className="devicon-bash-plain"></i>
+						<i className="skills-box-logo devicon-bash-plain"></i>
 						<p className="skills-box-text">Bash</p>
 					</div>
 
 
 					<div className="skills-box">
-						<i className="devicon-azure-plain"></i>
+						<i className="skills-box-logo devicon-azure-plain"></i>
 						<p className="skills-box-text">Azure</p>
 					</div>
 					<div className="skills-box">
-						<i className="devicon-heroku-original"></i>
+						<i className="skills-box-logo devicon-heroku-original"></i>
 						<p className="skills-box-text">Heroku</p>
 					</div>
 					<div className="skills-box">
-						<i className="fab fa-stack-overflow"></i>
+						<i className="skills-box-logo fab fa-stack-overflow"></i>
 						<p className="skills-box-text">Stack Overflow</p>
 					</div>
 					<div className="skills-box">
-						<i className="fab fa-github"></i>
+						<i className="skills-box-logo fab fa-github"></i>
 						<p className="skills-box-text">GitHub</p>
 					</div>
 					<div className="skills-box">
-						<i className="devicon-git-plain"></i>
+						<i className="skills-box-logo devicon-git-plain"></i>
 						<p className="skills-box-text">Git</p>
 					</div>
 					<div className="skills-box">
-						<i className="devicon-docker-plain"></i>
+						<i className="skills-box-logo devicon-docker-plain"></i>
 						<p className="skills-box-text">Docker</p>
 					</div>
 					<div className="skills-box">
-						<i className="devicon-mysql-plain"></i>
+						<i className="skills-box-logo devicon-mysql-plain"></i>
 						<p className="skills-box-text">MySQL</p>
 					</div>
 					<div className="skills-box">
-						<i className="devicon-visualstudio-plain"></i>
+						<i className="skills-box-logo devicon-visualstudio-plain"></i>
 						<p className="skills-box-text">Visual Studio</p>
 					</div>
 					<div className="skills-box">
-						<i className="devicon-vscode-plain"></i>
+						<i className="skills-box-logo devicon-vscode-plain"></i>
 						<p className="skills-box-text">Visual Studio Code</p>
 					</div>
 					<div className="skills-box">
-						<i className="devicon-putty-plain"></i>
+						<i className="skills-box-logo devicon-putty-plain"></i>
 						<p className="skills-box-text">PuTTy</p>
 					</div>
 					<div className="skills-box">
-						<i className="devicon-windows8-original"></i>
+						<i className="skills-box-logo devicon-windows8-original"></i>
 						<p className="skills-box-text">Windows 10</p>
 					</div>
 				</div>
